f95294a9c069764dc71fb03b6ef85e571fa30d32
[Taylor Dupont] <Taylorjodupont96@gmail.com>
2022-04-11 11:58:14 -0400

    styling login page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link class="homeLink" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link class="homeLink" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
 <style scoped>
+
+
 .homeLink{
   text-decoration: none;
   color: white;
@@ -15,15 +17,29 @@
   -webkit-text-stroke-color:rgb(0, 0, 0);
   
 }
+#login{
+
+ background-color:rgba(209, 255, 209, 0.8);
+  border-radius: 5px;
+  padding: 5px;
+  width: 30%;
+  margin: 5px;
+ 
+  }
+  
 #nav{
   background-color:rgba( 0, 100, 0, 0.5);
-  border-radius: 25px;
+  border-radius: 5px;
   padding: 5px;
-  text-align: center;
+  display: flex;
+  justify-content: flex-end;
+  margin: 5px;
 }
 #app{
-  
+  max-width: 100%;
   min-height: 100vh;
   background: url('images/golfCourse1.jpg') ;
+background-size:cover;
 }
+
 </style>
