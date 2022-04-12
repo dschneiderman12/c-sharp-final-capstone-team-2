351d2ad6c1755eb743ac7e5a5eb35366a16f3c30
[Taylor Dupont] <Taylorjodupont96@gmail.com>
2022-04-11 21:19:43 -0400

    style changes to login- colors, overlay, and shadows

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -23,60 +23,73 @@
   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
     "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
 }
-.homeLink {
-  text-decoration: none;
 
-  text-shadow: darkslateblue;
-  color: white;
-  font-size: x-large;
-  -webkit-text-stroke-width: 0.3px;
-  -webkit-text-stroke-color: rgb(0, 0, 0);
-  background-color: green;
-}
+
 #login {
-  background-color: rgba(209, 255, 209, 0.9);
-  border-color: darkgreen;
+  
+  background-color: rgba(209, 255, 209, 0.7);
+ 
+  border-color: rgb(43, 126, 43);
   border-style: solid;
-  border-width: 5px;
+  border-width: 3px;
   border-radius: 7px;
   padding: 5px;
   margin: 5px;
   width: 30%;
+
+  filter: drop-shadow(5px 5px 5px black);
+
 }
 
 #nav  > img
 {
-  max-width: 5%;
-  margin-right: 85%;
+  max-width: 7%;
+  /* margin-right: 80%; */
 }
 #nav {
-  /* height: 5vh; */
+
+/* this selects the top bar (logo, navigation buttons) */
+
   background-color: rgba(0, 100, 0, 0.5);
-  /* border-radius: 7px;
-  padding: 5px; */
+  /* border-radius: 7px;*/
+  padding: 5px; 
   
   display: flex;
   flex-direction: row;
  align-items: center;
-  /* justify-content: flex-end; */
+  justify-content: space-between;
   
 }
+
+
 #app {
   max-width: 100%;
   min-height: 100vh;
+  background-blend-mode: overlay;
   background: url("images/golfCourse1.jpg"),
-   linear-gradient(to bottom, #efe8f5, #8845e0);
+   linear-gradient(to bottom, #a1cc79, #9d5fb6);
   background-size: cover;
 }
-#nav > div {
+
+
+
+#nav > div > a{
+
+  /* this selects all of the navigation links */
   
-  /* display: flex;
-  justify-content: flex-end; */
-  /* width: 400px; */
-  /* margin-left: 90%; */
-}
-router-link {
   background-color: green;
-  height: 100px;
+  text-decoration: none;
+font-weight: bold;
+  text-shadow: darkslateblue 5px 5px 5px;
+  color: white;
+  font-size: x-large;
+  /* -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: rgb(0, 0, 0); */
+  
+  
+  margin-right: 5px;
+  padding:10px;
+  border-radius: 8px;
+
 }
 </style>
