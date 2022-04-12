d900dfdd996bb99cd669b41c112f5cdc507c45af
Daniel Schneiderman <dschneiderman12@gmail.com>
2022-04-12 11:26:51 -0400

    basic map embedded

---
+++ vue/src/components/CourseLocator.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <div class="map">
+  <iframe
+    width="800"
+    height="600"
+    frameborder="0"
+    style="border: 0"
+    referrerpolicy="no-referrer-when-downgrade"
+    src="https://www.google.com/maps/embed/v1/search?key=AIzaSyCMwjT48KCWWEad9HoL0IRtRAEG2rB96os&q=golf+course"
+    allowfullscreen
+  >
+  </iframe>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.map {
+    align-content: center;
+}
+iframe {
+    align-content: center;
+}
+</style>
