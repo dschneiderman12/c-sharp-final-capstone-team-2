de77936f689a9ad6e4f33caf3c3aeb00a22cde13
Daniel Schneiderman <dschneiderman12@gmail.com>
2022-04-19 12:40:44 -0400

    merge fix

--- vue/src/views/MatchInfo.vue
+++ vue/src/views/MatchInfo.vue
@@ -2,7 +2,6 @@
   <div>
     <div>
       <h2 v-on:created="populateLeague()">Match Name: {{ match.matchName }}</h2>
-<<<<<<< HEAD
       <tee-assignment-form
         v-if="league.organizerId === $store.state.user.userId"
       />
@@ -10,20 +9,6 @@
     <div>
       <match-score v-if="league.organizerId === $store.state.user.userId" />
     </div>
-=======
-      <p>{{ league.leagueId }}</p>
-
-      <tee-assignment-form
-        v-if="league.organizerId === $store.state.user.userId"
-      />
-    </div>
-    <div>
-      <!-- moved from leagueinfo.vue, not showing currently -->
-      <match-score
-        v-if="league.organizerId === $store.state.user.userId"
-      />
-    </div>
->>>>>>> 977b8dffa7ecc05da838f37fe9fe847add6f2bdc
   </div>
 </template>
 
@@ -61,19 +46,11 @@ export default {
             this.league = response.data;
           })
           .catch((error) => {
-<<<<<<< HEAD
             this.handleErrorResponse(error, "getting current league");
           });
       })
       .catch((error) => {
         this.handleErrorResponse(error, "getting current league");
-=======
-            this.handleErrorResponse(error, "gettingcurrentleague");
-          });
-      })
-      .catch((error) => {
-        this.handleErrorResponse(error, "gettingcurrentleague");
->>>>>>> 977b8dffa7ecc05da838f37fe9fe847add6f2bdc
       });
   },
   methods: {
