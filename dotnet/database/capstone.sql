b5eb23be9eca5a5aa20ff20fc670af1d15c6870e
Daniel Schneiderman <dschneiderman12@gmail.com>
2022-04-21 01:01:21 -0400

    database starting setup

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -49,7 +49,6 @@ CREATE TABLE matches (
 	CONSTRAINT [fk_league_id_match] FOREIGN KEY(league_id) REFERENCES leagues(league_id)
 
 )
-
 CREATE TABLE user_league(
 	user_id int NOT NULL,
 	league_id int NOT NULL
@@ -67,13 +66,6 @@ CREATE TABLE user_match(
 	CONSTRAINT [fk_user_id_match] FOREIGN KEY (user_id) REFERENCES users(user_id),
 	CONSTRAINT [fk-match_id] FOREIGN KEY (match_id) REFERENCES matches(match_id),
 )
---CREATE TABLE league_match(
---	league_id int NOT NULL,
---	match_id int NOT NULL,
---	CONSTRAINT [pk_league_match] PRIMARY KEY (league_id, match_id),
---	CONSTRAINT [fk_league_id_match] FOREIGN KEY(league_id) REFERENCES leagues(league_id),
---	
---)
 CREATE TABLE invites (
 	invite_id int NOT NULL IDENTITY(1,1),
 	invite_status varchar(30) NOT NULL,
@@ -83,14 +75,21 @@ CREATE TABLE invites (
 	CONSTRAINT [fk_to_user] FOREIGN KEY(to_user) REFERENCES users(user_id),
 	CONSTRAINT [fk_league_invite] FOREIGN KEY(to_league) REFERENCES leagues(league_id)
 )
+--CREATE TABLE league_match(
+--	league_id int NOT NULL,
+--	match_id int NOT NULL,
+--	CONSTRAINT [pk_league_match] PRIMARY KEY (league_id, match_id),
+--	CONSTRAINT [fk_league_id_match] FOREIGN KEY(league_id) REFERENCES leagues(league_id),
+--	
+--)
 --populate default data
-INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
-INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
-INSERT INTO courses (course_name, address) VALUES ('TEST COURSE 1', '1234 Test Lane Cleveland, OH');
-INSERT INTO leagues(league_name,organizer_id,course_id) VALUES ('TEST LEAGUE 1', 2, 1);
-INSERT INTO courses (course_name, address) VALUES ('Wildcard Golf National Challenge', '1234 Test Lane Cleveland, OH');
-INSERT INTO leagues (league_name,organizer_id,course_id) VALUES ('All Users',2,2);
-INSERT INTO invites (invite_status, to_user, to_league) VALUES ('pending', 1, 1); 
+--INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+--INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+--INSERT INTO courses (course_name, address) VALUES ('TEST COURSE 1', '1234 Test Lane Cleveland, OH');
+--INSERT INTO leagues(league_name,organizer_id,course_id) VALUES ('TEST LEAGUE 1', 2, 1);
+--INSERT INTO courses (course_name, address) VALUES ('Wildcard Golf National Challenge', '1234 Test Lane Cleveland, OH');
+--INSERT INTO leagues (league_name,organizer_id,course_id) VALUES ('All Users',2,2);
+--INSERT INTO invites (invite_status, to_user, to_league) VALUES ('pending', 1, 1); 
 -- UNCOMMENT AND RUN LINES 82-86 AFTER CREATING 3rd USER PER INSTRUCTIONS ON MERGE REQUEST
 
 --INSERT INTO user_league (user_id, league_id) VALUES (1,1), (3,1), (2,1);
